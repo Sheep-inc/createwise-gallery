854cb2dc3ed3e11aa5c3fedb628e88971b062192
Maarten Meijering <mkmeijering@student.che.nl>
2021-11-18 10:58:30 +0100

    no message

--- Templates/aframe.php
+++ Templates/aframe.php
@@ -17,24 +17,31 @@
 </script>
 <a-scene physics="debug: true">
   <a-assets>
-    <img id="my-image" src="https://picsum.photos/200/300">
+    <img id="my-image" src="https://picsum.photos/800/800">
+    <img id="imagee" src="https://t4.ftcdn.net/jpg/03/81/81/55/360_F_381815539_CUlqLBRjBFrFnkRNUGaF52eL5fNXSwrU.jpg">
   </a-assets>
 
   <a-entity id="cameraHolder" width="0" depth="0"   position="0 1.6 0">
     <!-- Camera Entity -->
     <a-entity id="camera" camera look-controls wasd-controls="acceleration: 100" kinematic-body></a-entity>
   </a-entity>
+    <a-box static-body="" position="-9.75 1.5 -4" color="#4CC3D9" width=".5" height="3" depth="10"></a-box>
+    <a-box static-body="" position="9.75 1.5 -4" color="#4CC3D9" width=".5" height="3" depth="10" ></a-box>
+    <a-box static-body="" position="0 1.5 -8.75" color="#4CC3D9" width="20" height="3" depth="0.5" ></a-box>
+    <a-box static-body="" position="0 1.5 1.2" color="#4CC3D9" width="20" height="3" depth="0.5" ></a-box>
+    <a-plane static-body="" rotation="-90 0 0" position="0 0 -4" width="20" height="10" color="#7BC8A4"></a-plane>
 
-  <!--<a-box static-body position="-4.1 1.5 -2.5" color="#4CC3D9" width=".5" height="3" depth="8"></a-box>-->
-  <!--<a-box static-body position="0 1.5 -6.5" color="#4CC3D9" width="8" height="3" depth="0.5"></a-box>-->
-  <a-box static-body="" position="-4.1 1.5 -2.5" color="#4CC3D9" width=".5" height="3" depth="8" material="" geometry="" velocity=""></a-box>
-  <a-box static-body="" position="0 1.5 -6.5" color="#4CC3D9" width="8" height="3" depth="0.5" material="" geometry="" velocity=""></a-box>
+    <a-image position="4.75 1.5 0.9" src="#my-image"></a-image>
+    <a-image position="-4.75 1.5 0.9" src="#my-image"></a-image>
+    <a-image position="0 1.5 0.9" src="#my-image"></a-image>
+    <a-image position="4.75 1.5 -8.4" src="#my-image"></a-image>
+    <a-image position="0 1.5 -8.4" src="#my-image"></a-image>
+    <a-image position="-4.75 1.5 -8.4" src="#my-image"></a-image>
 
-   <a-image position="0 1.5 -6.249" src="#my-image"></a-image>
-  <a-box static-body position="3.75 1.5 -2.8" color="#4CC3D9" width=".5" height="3" depth="8"></a-box>
-  <a-box static-body position="0 1.5 1.2" color="#4CC3D9" width="8" height="3" depth="0.5"></a-box>
-  <!-- <a-box dynamic-body position="0 1 -2" color="#4CC3D9" width="1" height="1" depth="1"></a-box> -->
+    <a-image position="9.4 1.5 -6.25" rotation="0 90 0" src="#my-image"></a-image>
+    <a-image position="9.4 1.5 -0.95" rotation="0 90 0" src="#my-image"></a-image>
+    <a-image position="-9.4 1.5 -6.25" rotation="0 90 0" src="#my-image"></a-image>
+    <a-image position="-9.4 1.5 -0.95" rotation="0 90 0" src="#my-image"></a-image>
 
-  <a-plane static-body rotation="-90 0 0" position="0 0 -4" width="20" height="10" color="#7BC8A4"></a-plane>
-  <a-sky color="#ECECEC"></a-sky>
+  <a-sky src="#imagee"></a-sky>
 </a-scene>
