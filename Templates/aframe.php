a8c8dd6aa75aca9afdeb8815e13a34c00e0c5616
Maarten Meijering <mkmeijering@student.che.nl>
2021-11-19 10:13:42 +0100

    Comit aframe.php

--- Templates/aframe.php
+++ Templates/aframe.php
@@ -17,19 +17,69 @@
 </script>
 <a-scene physics="debug: true">
   <a-assets>
-    <img id="my-image" src="https://picsum.photos/800/800">
-    <img id="imagee" src="https://t4.ftcdn.net/jpg/03/81/81/55/360_F_381815539_CUlqLBRjBFrFnkRNUGaF52eL5fNXSwrU.jpg">
+    <img id="my-image" src="https://picsum.photos/400/400">
+    <img id="sky" src="https://t4.ftcdn.net/jpg/03/81/81/55/360_F_381815539_CUlqLBRjBFrFnkRNUGaF52eL5fNXSwrU.jpg">
+    <img id="wall" src="/wp-content/themes/createwise-gallery/images/Concrete.jpg">
   </a-assets>
-
-  <a-entity id="cameraHolder" width="0" depth="0"   position="0 1.6 0">
+  <a-entity id="cameraHolder" width="0" depth="0"  position="0 1.6 0">
     <!-- Camera Entity -->
-    <a-entity id="camera" camera look-controls wasd-controls="acceleration: 100" kinematic-body></a-entity>
+    <a-entity id="camera" camera look-controls wasd-controls="acceleration: 300" kinematic-body></a-entity>
   </a-entity>
-    <a-box static-body="" position="-9.75 1.5 -4" color="#4CC3D9" width=".5" height="3" depth="10"></a-box>
-    <a-box static-body="" position="9.75 1.5 -4" color="#4CC3D9" width=".5" height="3" depth="10" ></a-box>
-    <a-box static-body="" position="0 1.5 -8.75" color="#4CC3D9" width="20" height="3" depth="0.5" ></a-box>
-    <a-box static-body="" position="0 1.5 1.2" color="#4CC3D9" width="20" height="3" depth="0.5" ></a-box>
-    <a-plane static-body="" rotation="-90 0 0" position="0 0 -4" width="20" height="10" color="#7BC8A4"></a-plane>
+    <a-box
+      static-body=""
+      position="-9.75 1.5 -4"
+      width=".5" height="3" depth="10"
+      src="#wall"
+      repeat="2 1"
+      normal-map="#wall"
+      normal-texture-repeat="2 1"
+    ></a-box>
+
+    <a-box
+      static-body=""
+      position="9.75 1.5 -4"
+      width=".5" height="3" depth="10"
+      src="#wall"
+      repeat="2 1"
+      normal-map="#wall"
+      normal-texture-repeat="2 1"
+      roughness="0.8"
+      normal-scale="-4 1"
+
+    ></a-box>
+
+    <a-box
+      static-body=""
+      position="0 1.5 -8.75"
+      width="20" height="3" depth="0.5"
+      src="#wall"
+      repeat="3 1"
+      normal-map="#wall"
+      normal-texture-repeat="3 1"
+      roughness="0.8"
+    ></a-box>
+
+    <a-box
+      static-body=""
+      position="0 1.5 1.2"
+      width="20" height="3" depth="0.5"
+      src="#wall"
+      repeat="3 1"
+      normal-map="#wall"
+      normal-texture-repeat="3 1"
+      roughness="0.8"
+    ></a-box>
+
+    <a-plane
+      static-body=""
+      rotation="-90 0 0"
+      position="0 0 -4" width="20" height="10"
+      src="#wall"
+      repeat="3 2"
+      normal-map="#wall"
+      normal-texture-repeat="3 2"
+      roughness="0.8"
+    ></a-plane>
 
     <a-image position="4.75 1.5 0.9" src="#my-image"></a-image>
     <a-image position="-4.75 1.5 0.9" src="#my-image"></a-image>
@@ -43,5 +93,5 @@
     <a-image position="-9.4 1.5 -6.25" rotation="0 90 0" src="#my-image"></a-image>
     <a-image position="-9.4 1.5 -0.95" rotation="0 90 0" src="#my-image"></a-image>
 
-  <a-sky src="#imagee"></a-sky>
+  <a-sky src="#sky"></a-sky>
 </a-scene>
