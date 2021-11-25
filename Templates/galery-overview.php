197cc68b1e2fecba579cf1149db81716d600dd41
Maarten Meijering <mkmeijering@student.che.nl>
2021-11-25 10:19:03 +0100

    Update style sheet href

--- Templates/galery-overview.php
+++ Templates/galery-overview.php
@@ -2,7 +2,8 @@
 <html>
 
   <head>
-    <link rel="stylesheet"  href="/wp-content/plugins/createwise-gallery/style/style.css">
+    <link rel="stylesheet"  href="/wp-content/themes/createwise-gallery/style/style.css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Galerij_Overzicht</title>
   </head>
@@ -14,16 +15,16 @@
            <input id="searchbar" placeholder="Zoeken...">
          </div>
          <div class="searchOptions col-sm-4 col-md-2">
-           <select>
+           <select name= "order">
              <option>a-z</option>
              <option>z-a</option>
              <option>populairiteit</option>
            </select>
          </div>
        </div>
-       <div class="galleryHolder">
+       <div id="galleryHolder">
          <div class="card galleryCard col-md-4 col-sm-12">
-           <a href="ttps://nl.wiki.forgeofempires.com/index.php?title=Van_Gogh">
+           <a href="https://nl.wiki.forgeofempires.com/index.php?title=Van_Gogh">
              Van Gogh:<br>
              <img src="https://nl.wiki.forgeofempires.com/images/8/89/VAN-GOGH.png" class="card-img-top" alt="...">
              </a>
@@ -32,12 +33,17 @@
              </div>
          </div>
 
-         Enzo knol:<br>
-         <a href="https://www.youtube.com/watch?v=OXyxn3YS8sg">
-            <img alt="Thumbnail"style="width:10%;height:10%" src="https://i.ytimg.com/vi/OXyxn3YS8sg/mqdefault.jpg">
-         </a>
-         <p>Sjoerd Janssen</p>
+         <div class="card galleryCard col-md-4 col-sm-12">
+           <a href="https://www.youtube.com/watch?v=OXyxn3YS8sg">
+             Enzo Knol:<br>
+             <img src="https://i.ytimg.com/vi/OXyxn3YS8sg/mqdefault.jpg" class="card-img-top" alt="...">
+             </a>
+             <div class="card-body">
+               <h5 class="card-title"><a href="/users/Sjoerd Janssen">Sjoerd Janssen</a></h5>
+             </div>
+         </div>
      </div>
   </div>
+  <script refer src="/wp-content/themes/createwise-gallery/scripts/galleryOverview.js"></script>
   </body>
 </html>
